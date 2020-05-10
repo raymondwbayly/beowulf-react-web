5ea929f31e1442a448d0c29ff3b347af2e3b7423
Raymond Bayly <raymond.bayly@outlook.com>
2020-05-10 13:15:13 -0400

    update the login

--- components/FPCard/index.jsx
+++ components/FPCard/index.jsx
@@ -36,7 +36,13 @@ class FPCard extends Component {
                     label="Send my Password"
                     primary={true}
                     styleName="LoginButton"
-                    onClick={(event) => this.handleClick(event)}
+                    onClick={(event) => this.sendPassword(event)}
+                  />
+                  <RaisedButton
+                    label="Cancel"
+                    primary={false}
+                    styleName="CancelButton"
+                    onClick={(event) => this.cancelEvent(event)}
                   />
                 </div>
               </MuiThemeProvider>
@@ -48,6 +54,41 @@ class FPCard extends Component {
       </div>
     );
   }
+
+  sendPassword(event){
+    var apiBaseUrl = "http://localhost:4000/api/";
+    var self = this;
+    var payload={
+    "email":this.state.username,
+    "password":this.state.password
+    }
+    axios.post(apiBaseUrl+'login', payload)
+    .then(function (response) {
+    console.log(response);
+    if(response.data.code == 200){
+    console.log("Login successfull");
+    var uploadScreen=[];
+    uploadScreen.push(<UploadScreen appContext={self.props.appContext}/>)
+    self.props.appContext.setState({loginPage:[],uploadScreen:uploadScreen})
+    }
+    else if(response.data.code == 204){
+    console.log("Username password do not match");
+    alert("username password do not match")
+    }
+    else{
+    console.log("Username does not exists");
+    alert("Username does not exist");
+    }
+    })
+    .catch(function (error) {
+    console.log(error);
+    });
+    }
+
+    cancelEvent(event){
+        window.location.href="/";
+      }
+
 }
 
 export default FPCard;
--- components/LoginCard/index.jsx
+++ components/LoginCard/index.jsx
@@ -44,7 +44,7 @@ class LoginCard extends Component {
                     label="Submit"
                     primary={true}
                     styleName="LoginButton"
-                    onClick={(event) => this.handleClick(event)}
+                    onClick={(event) => this.loginUser(event)}
                   />
                 </div>
               </MuiThemeProvider>
@@ -59,6 +59,36 @@ class LoginCard extends Component {
       </div>
     );
   }
+
+  loginUser(event){
+    var apiBaseUrl = "http://localhost:3100/api/";
+    var self = this;
+    var payload={
+    "email":this.state.username,
+    "password":this.state.password
+    }
+    axios.post(apiBaseUrl+'login', payload)
+    .then(function (response) {
+    console.log(response);
+    if(response.data.code == 200){
+    console.log("Login successfull");
+    var uploadScreen=[];
+    uploadScreen.push(<UploadScreen appContext={self.props.appContext}/>)
+    self.props.appContext.setState({loginPage:[],uploadScreen:uploadScreen})
+    }
+    else if(response.data.code == 204){
+    console.log("Username password do not match");
+    alert("username password do not match")
+    }
+    else{
+    console.log("Username does not exists");
+    alert("Username does not exist");
+    }
+    })
+    .catch(function (error) {
+    console.log(error);
+    });
+    }
 }
 
 export default LoginCard;
